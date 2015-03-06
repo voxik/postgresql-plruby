9256713f58ff9be26a9baace94a05fd4c32fcbe0
Devrim Gündüz <devrim@gunduz.org>
2015-03-07 01:52:10 +0200

    Complain if script is sourced in psql, rather than via CREATE EXTENSION

--- extension/plruby--0.5.5.sql
+++ extension/plruby--0.5.5.sql
@@ -1,3 +1,6 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION plruby" to load this file. \quit
+
 CREATE FUNCTION plruby_call_handler () RETURNS language_handler
 	AS '$libdir/plruby'
         language C;
